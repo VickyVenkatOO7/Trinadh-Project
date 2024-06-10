22723c12cdc4a40215698ba16155adb420b70a9f
Trinadh Sai Rapeti <116419508+VickyVenkatOO7@users.noreply.github.com>
2024-06-10 22:05:10 +0530

    Add files via upload
    Made delivery options Interactive

--- JAVASCRIPT-AMAZON-PROJECT/data/cart.js
+++ JAVASCRIPT-AMAZON-PROJECT/data/cart.js
@@ -48,3 +48,17 @@ export function removeFromCart(productId) {
     cart = newCart;
     saveToStorage();
 }
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+    let matchingItem;
+
+    cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+            matchingItem = cartItem;
+        }
+    });
+
+    matchingItem.deliveryOptionId = deliveryOptionId;
+
+    saveToStorage();
+}
--- JAVASCRIPT-AMAZON-PROJECT/scripts/checkout.js
+++ JAVASCRIPT-AMAZON-PROJECT/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import { cart, removeFromCart, updateDeliveryOption } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
@@ -90,14 +90,16 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
         );
         const dateString = deliveryDate.format('dddd MMMM D');
 
-        const priceString = deliveryDate.price ===0
+        const priceString = deliveryDate.price === 0
             ? 'FREE'
             : `₹${formatCurrency(deliveryOption.price)} -`;
 
         const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
-            
+
         html += `
-            <div class="delivery-option">
+            <div class="delivery-option js-delivery-option" 
+              data-product-id=${matchingProduct.id}
+              data-delivery-option-id="${deliveryOption.id}">
                 <input type="radio"
                     ${isChecked ? 'checked' : ''}
                     class="delivery-option-input"
@@ -129,5 +131,13 @@ document.querySelectorAll('.js-delete-link')
                 .js-cart-item-container-${productId}
             `);
             container.remove();
+        });
+    });
+
+document.querySelectorAll('.js-delivery-option')
+    .forEach((element) => {
+        element.addEventListener('click', () => {
+            const { productId, deliveryOptionId } = element.dataset;
+            updateDeliveryOption(productId, deliveryOptionId);
         });
     });
