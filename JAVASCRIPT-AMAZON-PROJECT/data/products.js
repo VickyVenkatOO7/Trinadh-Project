8059b0bff1dd91dcf6d5193577f743ebdbc60a7e
Trinadh Sai Rapeti <116419508+VickyVenkatOO7@users.noreply.github.com>
2024-06-19 17:55:10 +0530

    Add files via upload
    Used Inheritance Concept

--- JAVASCRIPT-AMAZON-PROJECT/data/products.js
+++ JAVASCRIPT-AMAZON-PROJECT/data/products.js
@@ -34,6 +34,25 @@ class Product {
   getPrice() {
     return `₹${formatCurrency(this.price)}`;
   }
+
+  extraInfoHTML() {
+    return '';
+  }
+}
+
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">Size Chart</a>
+    `
+  }
 }
 
 export const products = [
@@ -696,6 +715,9 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if (productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
 
--- JAVASCRIPT-AMAZON-PROJECT/scripts/amazon.js
+++ JAVASCRIPT-AMAZON-PROJECT/scripts/amazon.js
@@ -43,6 +43,8 @@ products.forEach((product) => {
             </select>
             </div>
 
+            ${product.extraInfoHTML()}
+
             <div class="product-spacer"></div>
 
             <div class="added-to-cart">
